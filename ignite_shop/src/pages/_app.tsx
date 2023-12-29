08d2fff06a91b0083d6a996f838c17ee2144da5c
adhmattheus <mattheusadhonnaysf@hotmail.com>
2023-12-29 14:31:58 -0300

    estilo global

--- ignite_shop/src/pages/_app.tsx
+++ ignite_shop/src/pages/_app.tsx
@@ -1,5 +1,9 @@
-import type { AppProps } from 'next/app'
+import { globalStyles } from "../styles/global"
+import { AppProps } from 'next/app'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
+
   return <Component {...pageProps} />
 }
