8e4713b92179a2575313ddd501681b5d7f5467f8
adhmattheus <mattheusadhonnaysf@hotmail.com>
2023-12-29 13:25:52 -0300

    configurando fonte e estilo

--- ignite_shop/src/pages/index.tsx
+++ ignite_shop/src/pages/index.tsx
@@ -1,5 +1,11 @@
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  backgroundColor: '$green300',
+})
+
 export default function Home() {
   return (
-    <h1>hello world</h1>
+    <Button>enviar</Button>
   )
 }
