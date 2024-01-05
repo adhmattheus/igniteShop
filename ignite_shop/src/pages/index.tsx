223b11bfa5b8b137529cfaf1b7d5e4ccefea904b
adhmattheus <mattheusadhonnaysf@hotmail.com>
2024-01-05 10:07:01 -0300

    estrutura da página de produtos

--- ignite_shop/src/pages/index.tsx
+++ ignite_shop/src/pages/index.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { stripe } from "@/lib/stripe";
 import Stripe from "stripe";
 import { GetStaticProps } from "next";
+import Link from "next/link";
 
 interface HomeProps {
   products: {
@@ -28,14 +29,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Product className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
 
